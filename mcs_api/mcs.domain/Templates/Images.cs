9f84a77db28827abb0995f800b0a82a0dbacdda3
Nasar Eddaoui <nasar165@hotmail.com>
2019-12-02 19:05:41 +0700

    Convert repo as a core

--- mcs_api/mcs.domain/Templates/Images.cs
+++
@@ -1,9 +0,0 @@
-namespace mcs.domain.Templates
-{
-    public class Images
-    {
-        public int Ref_Id { get; set; }
-        public int Image_Id { get; set; }
-        public string FileLocation { get; set; }
-    }
-}
