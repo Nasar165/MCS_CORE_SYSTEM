846183598eb2cf6830697873589a001ab663c2b1
Nasar Eddaoui <nasar165@hotmail.com>
2019-12-27 00:57:28 +0700

    #10 Add AES code unclean

--- mcs_api/mcs.components/AesEncrypter.cs
+++ mcs_api/mcs.components/AesEncrypter.cs
@@ -1,20 +1,64 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
 public class AesEncrypter : IEncrypter
 {
-    //Implement AES Encryption
     protected string SymmetricKey {get;set;}
-    public AesEncrypter()
-    {
-        
-    }
+    public AesEncrypter(string symmetricKey)
+        => SymmetricKey = symmetricKey;
 
     public string EncryptData(string valueToEncrypt)
     {
-        throw new System.NotImplementedException();
+        byte[] iv = new byte[16];  
+        byte[] array;  
+
+        using (Aes aes = Aes.Create())  
+        {  
+            aes.Key = Encoding.UTF8.GetBytes(SymmetricKey);  
+            aes.IV = iv;  
+
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);  
+
+            using (MemoryStream memoryStream = new MemoryStream())  
+            {  
+                using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))  
+                {  
+                    using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))  
+                    {  
+                        streamWriter.Write(valueToEncrypt);  
+                    }  
+
+                    array = memoryStream.ToArray();  
+                }  
+            }  
+        }  
+        return Convert.ToBase64String(array);  
     }
 
-    public string DecryptyData<T>(string valueToDecrypt)
+    public string DecryptyData(string valueToDecrypt)
     {
-        throw new System.NotImplementedException();
+        byte[] iv = new byte[16];  
+        byte[] buffer = Convert.FromBase64String(valueToDecrypt);  
+
+        using (Aes aes = Aes.Create())  
+        {  
+            aes.Key = Encoding.UTF8.GetBytes(SymmetricKey);  
+            aes.IV = iv;  
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);  
+
+            using (MemoryStream memoryStream = new MemoryStream(buffer))  
+            {  
+                using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))  
+                {  
+                    using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))  
+                    {  
+                        return streamReader.ReadToEnd();  
+                    }  
+                }  
+            }  
+        }
     }
 
 
