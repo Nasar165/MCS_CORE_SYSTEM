3c4142b8d8cc1d6e9db9ca139e02f4a62d04dfb0
Nasar Eddaoui <nasar165@hotmail.com>
2019-12-29 22:31:41 +0700

    #10 Ecrypt password before testing

--- mcs_api/mcs.api/Security/AuthTemplate/UserAccount.cs
+++ mcs_api/mcs.api/Security/AuthTemplate/UserAccount.cs
@@ -1,15 +1,30 @@
 using System.ComponentModel.DataAnnotations;
+using mcs.api.Models;
 using mcs.api.Security.AuthTemplate.Interface;
 
 namespace mcs.api.Security.AuthTemplate
 {
     public sealed class UserAccount : DataExtension, IUserAccount
     {
+        IEncrypter encrypter {get;}
+        public UserAccount()
+            => encrypter = new AesEncrypter(
+                AppConfigHelper.Instance.GetValueFromAppConfig("AppSettings","SymmetricKey"));
         public int UserAccount_Id { get; set; }
         [Required]
-        [StringLength(11, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
+        [StringLength(11, ErrorMessage = "The {0} value cannot exceed {1} characters.")]
         public string Username { get; set; }
+        private string password {get;set;}
         [Required]
-        public string Password { get; set; }
+        public string Password { 
+            get
+            {
+                return password;
+            } 
+            set 
+            {   if(value != null)
+                    password = encrypter.EncryptData(value);
+            }
+        }
     }
 }
