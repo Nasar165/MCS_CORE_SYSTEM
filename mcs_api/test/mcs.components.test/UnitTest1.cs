bf54d74aa96cdee6a0eef46df001b6632b62cba8
Nasar Eddaoui <nasar165@hotmail.com>
2019-12-31 22:35:01 +0700

    #11 fix add unit test

---
+++ mcs_api/test/mcs.components.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mcs.components.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
