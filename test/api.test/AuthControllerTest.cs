dc17b208e94c89b84f20a46b5f51dfc0145e2435
Nasar Eddaoui <nasar165@hotmail.com>
2020-01-22 03:31:03 +0700

    #27 add unit test API

---
+++ test/api.test/AuthControllerTest.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace api.test
+{
+    [TestClass]
+    public class AuthControllerTest
+    {
+        [TestMethod]
+        public void AuthenticateUser()
+        {
+            
+        }
+
+         [TestMethod]
+        public void AuthenticateAPI()
+        {
+
+        }
+
+    }
+}
